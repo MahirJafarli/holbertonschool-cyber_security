ed323c478c607ad13d675581c11a790853bf13ca
MahirJafarli <jafarlimahir@gmail.com>
2026-05-16 02:01:00 +0000

    Fix

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -type d -perm -0002 -not -path "/proc/*" -not -path "/sys/*" -print -exec chmod o-w {} \; 2>/dev/null
+find / -type d -perm -0002 -print -exec chmod 775 {} \; 2>/dev/null

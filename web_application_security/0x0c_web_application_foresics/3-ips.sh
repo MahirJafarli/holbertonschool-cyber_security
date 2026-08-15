b9bea2c2c58a2fc88192683b69706c15adf3c978
MahirJafarli <jafarlimahir@gmail.com>
2026-08-16 00:10:31 +0400

    FIx

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "Accepted" ${1:-auth.log} | grep -oP '\b(?:\d{1,3}\.){3}\d{1,3}\b' | sort -u | wc -l
+grep "Failed" ${1:-auth.log} | grep -oP '\b(?:\d{1,3}\.){3}\d{1,3}\b' | sort -u | wc -l
